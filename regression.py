bfb8c3a1dfaf639e34d6ed46954d793695f3d731
dredwardhyde <komesergey@gmail.com>
2021-01-30 18:52:31 +0300

    Refactoring

--- regression.py
+++ regression.py
@@ -3,6 +3,8 @@ from matplotlib import pyplot as plt
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 np.random.seed(1)
 
@@ -14,7 +16,7 @@ def f(x):
 
 # ============================================= GAUSSIAN KERNELS =======================================================
 kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-
+poly_degree = 7
 # ============================================== WITHOUT NOISE =========================================================
 X = np.reshape([1., 3., 5., 6., 7., 8.], (1, -1)).T
 y = f(X).reshape(-1)
@@ -24,6 +26,13 @@ gp.fit(X, y)
 
 x = np.reshape(np.linspace(0, 10, 1000), (1, -1)).T
 y_pred, sigma = gp.predict(x, return_std=True)
+
+poly = PolynomialFeatures(poly_degree)
+X_transform = poly.fit_transform(np.reshape(X, (-1, 1)).astype(np.float32))
+lm = LinearRegression()
+lm.fit(X_transform,  np.reshape(y, (-1, 1)).astype(np.float32))
+X_test_transform = poly.fit_transform(np.reshape(x, (-1, 1)))
+y_pred_linear = lm.predict(X_test_transform)
 
 # ============================================== PLOT RESULTS ==========================================================
 plt.figure()
@@ -36,6 +45,7 @@ plt.fill(np.concatenate([x, x[::-1]]),
          alpha=.5, fc='b', ec='None', label='95% confidence interval')
 plt.xlabel('$x$')
 plt.ylabel('$f(x)$')
+plt.plot(x, y_pred_linear, 'c', label='Polynomial regression N={}'.format(poly_degree))
 plt.ylim(-10, 20)
 plt.legend(loc='upper left')
 
@@ -50,6 +60,14 @@ gp.fit(X, y)
 
 y_pred, sigma = gp.predict(x, return_std=True)
 
+
+poly = PolynomialFeatures(poly_degree)
+X_transform = poly.fit_transform(np.reshape(X, (-1, 1)).astype(np.float32))
+lm = LinearRegression()
+lm.fit(X_transform,  np.reshape(y, (-1, 1)).astype(np.float32))
+X_test_transform = poly.fit_transform(np.reshape(x, (-1, 1)))
+y_pred_linear = lm.predict(X_test_transform)
+
 # ============================================== PLOT RESULTS ==========================================================
 plt.figure()
 plt.plot(x, f(x), 'r:', label=r'$f(x) = x\,\sin(x)$')
@@ -61,6 +79,7 @@ plt.fill(np.concatenate([x, x[::-1]]),
          alpha=.5, fc='b', ec='None', label='95% confidence interval')
 plt.xlabel('$x$')
 plt.ylabel('$f(x)$')
+plt.plot(x, y_pred_linear, 'c', label='Polynomial regression N={}'.format(poly_degree))
 plt.ylim(-10, 20)
 plt.legend(loc='upper left')
 plt.show()
