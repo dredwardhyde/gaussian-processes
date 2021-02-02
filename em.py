3685d940dbd9f0ff85663f79cfd668efd9a32ea2
Nikita L <komesergey@gmail.com>
2021-02-02 22:33:54 +0300

    Refactoring

--- em.py
+++ em.py
@@ -6,7 +6,6 @@ from scipy.stats import multivariate_normal
 def e_step(X, means, covs, mixing_coefs):
     responsibilities = np.zeros((X.shape[0], means.shape[0]))
     num = np.zeros(mixing_coefs.shape[0])
-    denomin = 0
     for n in range(X.shape[0]):
         denomin = 0
         for k in range(mixing_coefs.shape[0]):
@@ -35,9 +34,7 @@ def m_step(X, responsibilities):
             sum_cov += responsibilities[i, k] * np.outer(A, A.T)
         means[k, :] = sum_mean
         covs[k, :, :] = sum_cov / N[k]
-    NN = N.sum(axis=0)
-    mixing_coefs = N / NN
-
+    mixing_coefs = N / N.sum(axis=0)
     return means, covs, mixing_coefs
 
 
@@ -45,12 +42,12 @@ def plot_gmm_2d(X, responsibilities, means, covs, mixing_coefs):
     plt.figure(figsize=[6, 6])
     palette = [[1, 0, 0], [0, 0, 1]]
     colors = responsibilities.dot(palette)
-    # Plot the samples colored according to p(z|x)
+    # plot the samples colored according to p(z|x)
     plt.scatter(X[:, 0], X[:, 1], c=colors, alpha=0.5)
-    # Plot locations of the means
+    # plot locations of the means
     for ix, m in enumerate(means):
         plt.scatter(m[0], m[1], s=150, marker='X', color=palette[ix], edgecolors='k', linewidths=1, )
-    # Plot contours of the Gaussian
+    # plot contours of the Gaussian
     x, y = np.mgrid[0:1:0.02, 0:1:0.02]
     pos = np.dstack((x, y))
     for k in range(len(mixing_coefs)):
@@ -67,14 +64,14 @@ X_norm[:, 0] = (X[:, 0] - np.amin(X[:, 0])) / (np.amax(X[:, 0]) - np.amin(X[:, 0
 X_norm[:, 1] = (X[:, 1] - np.amin(X[:, 1])) / (np.amax(X[:, 1]) - np.amin(X[:, 1]))
 max_iters = 20
 
-# Initialize the parameters
+# initialize parameters
 means = np.array([[0.2, 0.6], [0.8, 0.4]])
 covs = np.array([0.5 * np.eye(2), 0.5 * np.eye(2)])
 mixing_coefs = np.array([0.5, 0.5])
 responsibilities = e_step(X_norm, means, covs, mixing_coefs)
 plot_gmm_2d(X_norm, responsibilities, means, covs, mixing_coefs)
 
-# Perform the EM iteration
+# perform the EM iteration
 for i in range(max_iters):
     responsibilities = e_step(X_norm, means, covs, mixing_coefs)
     means, covs, mixing_coefs = m_step(X_norm, responsibilities)
